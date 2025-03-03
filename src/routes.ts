84be79d7f5071c3a239e75603dc8e5d6d1ada030
Christian J. Dennis <aottgpvp@gmail.com>
2025-03-03 15:45:15 -0400

    initial commit

---
+++ src/routes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import pool from "./db";
+
+const router = Router();
+
+router.get("/", async(req,res)=>{ res.json({'rsp':'hello'})});
+
+// Test database connection
+router.get("/users", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM users");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+});
+
+export default router;
