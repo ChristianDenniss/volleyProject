912d61d6da15664ca5fadb6626be0ef6cbcfe5d1
Christian J. Dennis <aottgpvp@gmail.com>
2025-06-18 02:28:31 -0300

    removed video url restriction so we can set to N/A in admin panel

--- BE/src/modules/games/games.schema.ts
+++ BE/src/modules/games/games.schema.ts
@@ -8,7 +8,7 @@ export const createGameSchema = z.object({
     team1Score: z.number().int().positive(),
     team2Score: z.number().int().positive(),
     stats: z.array(z.number().int().positive().optional()),
-    videoUrl: z.string().url({ message: "Video URL is required and must be a valid URL" }).optional(),
+    videoUrl: z.string().optional(),
     stage: z.string().min(2, { message: "stage is required, min 2 characters" }),
 });
 
