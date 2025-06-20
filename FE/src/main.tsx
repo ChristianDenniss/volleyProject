6b83370147ae546e4e5b7af04ed57ca39f583c82
Christian J. Dennis <aottgpvp@gmail.com>
2025-06-20 17:16:03 -0300

    new seo provider

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HelmetProvider } from '@dr.pogodin/react-helmet'
 import './styles/App.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </StrictMode>,
 )
