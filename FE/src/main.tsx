537f3b88c44c1d61c0e92e3647cddc4f87e9752e
Christian J. Dennis <aottgpvp@gmail.com>
2025-06-22 17:49:22 -0300

    adding static cache busting, fixing filter color, adding filter?

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from '@dr.pogodin/react-helmet'
 import './styles/App.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <HelmetProvider>
       <App />
     </HelmetProvider>
-  </StrictMode>,
+  </React.StrictMode>,
 )
