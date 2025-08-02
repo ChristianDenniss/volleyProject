ce82ac6527206213ab17607516c39db229109da7
Christian J. Dennis <aottgpvp@gmail.com>
2025-08-02 20:25:43 -0300

    adding schedules / matches

--- FE/src/components/Schedules.tsx
+++ FE/src/components/Schedules.tsx
@@ -158,7 +158,11 @@ const Schedules: React.FC = () => {
           <button className="nav-arrow" onClick={() => setCurrentPage(Math.min(totalPages, currentPage + 1))}>
             ›
           </button>
-          <button className="calendar-btn">📅</button>
+                     <button className="calendar-btn">
+             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+               <path d="M8 2V6M16 2V6M3 10H21M5 4H19C20.1046 4 21 4.89543 21 6V20C21 21.1046 20.1046 22 19 22H5C3.89543 22 3 21.1046 3 20V6C3 4.89543 3.89543 4 5 4Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+             </svg>
+           </button>
         </div>
 
         <div className="filter-row">
@@ -261,16 +265,7 @@ const Schedules: React.FC = () => {
             </select>
           </div>
 
-          <div className="filter-group">
-            <label>
-              <input
-                type="checkbox"
-                checked={showLocalTime}
-                onChange={(e) => setShowLocalTime(e.target.checked)}
-              />
-              Show local match time
-            </label>
-          </div>
+
 
           {(searchQuery || selectedRound) && (
             <button className="clear-filters-button" onClick={clearFilters}>
