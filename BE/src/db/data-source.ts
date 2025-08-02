4733602b973047d5721d4ac5dd5879a4cff15b19
Christian J. Dennis <aottgpvp@gmail.com>
2025-08-02 18:36:50 -0300

    adding schedules / matches

--- BE/src/db/data-source.ts
+++ BE/src/db/data-source.ts
@@ -18,6 +18,7 @@ import { User } from "../modules/user/user.entity.js";
 import { Article } from "../modules/articles/article.entity.js";
 import { Awards } from "../modules/awards/award.entity.js";
 import { Records } from "../modules/records/records.entity.js";
+import { Matches } from "../modules/matches/match.entity.js";
 
 dotenv.config();
 
@@ -31,7 +32,8 @@ const entities = [
     User,
     Article,
     Awards,
-    Records
+    Records,
+    Matches
 ];
 
 // Configure AppDataSource
